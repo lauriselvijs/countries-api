02fce86ead5523a79d2f39f75dcd401668c0321c
Lauris Mačāns <85683069+lauriselvijs@users.noreply.github.com>
2022-04-18 14:19:15 +0300

    Update Root.component.tsx

--- src/route/Root/Root.component.tsx
+++ src/route/Root/Root.component.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import DarkModeBtn from "../../component/DarkModeBtn";
 import HeaderWrapper from "../../component/HeaderWrapper";
 import Title from "../../component/Title";
+import "./Root.style.scss"
 
 const Root = () => {
   return (
